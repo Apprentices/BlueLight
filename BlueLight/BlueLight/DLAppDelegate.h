379b2f4bf5e20573ef1a7fe60496263aa8f77231
ThisIsTaylor <taylor.mcgregor@detroitlabs.com>
2014-08-06 12:26:17 -0400

    Created base for xcode proj

---
+++ BlueLight/BlueLight/DLAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  DLAppDelegate.h
+//  BlueLight
+//
+//  Created by DL Admin on 8/6/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic) UIWindow *window;
+@property (nonatomic) UINavigationController *navController;
+
+@end
