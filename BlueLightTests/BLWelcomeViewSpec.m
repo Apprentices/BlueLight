2afc46571c900736ae40d929c9a3ce3ab390476d
ThisIsTaylor <taylor.mcgregor@detroitlabs.com>
2014-08-07 14:19:24 -0400

    Wrote test for signUpButton

--- BlueLightTests/BLWelcomeViewSpec.m
+++ BlueLightTests/BLWelcomeViewSpec.m
@@ -18,12 +18,20 @@
 
 SPEC_BEGIN(BLWelcomeViewSpec)
 
-	describe(@"BLWelcomeView", ^{
-		it(@"should have a sign up button", ^{
-            UIButton *signUpButton = [UIButton nullMock];
-            [[signUpButton shouldNot] beNil];
-        });
+describe(@"BLWelcomeView", ^{
+    it(@"should have a sign up button", ^{
+        UIButton *signUpButton = [UIButton nullMock];
+        [[signUpButton shouldNot] beNil];
+    });
+    
+    it(@"should go to sign up page when signUpButton is tapped", ^{
+        BLWelcomeView *welcomeView = [[BLWelcomeView alloc] init];
+        BLSignUpViewController *signUpViewController = [BLSignUpViewController nullMock];
+        UINavigationController *navController = [UINavigationController nullMock];
+        
+        [navController pushViewController:signUpViewController animated:YES];
+        [[welcomeView should]respondToSelector:@selector(signUpButtonPressed:)];
+    });
+});
 
-	});
-	
 SPEC_END
