010b92ccccd21538c3847269842b88bb56e24e3f
ThisIsTaylor <taylor.mcgregor@detroitlabs.com>
2014-08-07 10:55:32 -0400

    Created SignIn and SignUpViewControllers

---
+++ BlueLight/BLSignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLSignInViewController.h
+//  BlueLight
+//
+//  Created by DL Admin on 8/7/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLSignInViewController : UIViewController
+
+@end
---
+++ BlueLight/BLSignUpViewController.m
@@ -0,0 +1,38 @@
+//
+//  BLSignUpViewController.m
+//  BlueLight
+//
+//  Created by DL Admin on 8/7/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import "BLSignUpViewController.h"
+
+@interface BLSignUpViewController ()
+
+@end
+
+@implementation BLSignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
